8b1cdd84884f840a726ed1a872c78d45b2ae099b
Hussein <akanjiabayomi2@gmail.com>
2024-01-03 14:27:57 -0800

    python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+"""define a class Square"""
+
+class Square:
+    """represent a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+"""define a class Square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args: size (int): The size of the new square.
+        """
+        self.__size = size
