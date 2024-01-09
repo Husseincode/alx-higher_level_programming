a93b57d36ba7a1f51d5ddde236e61c4832923b68
Hussein <akanjiabayomi2@gmail.com>
2024-01-09 09:02:47 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def write_file(filename="", text=""):
     """
-    Write a string to a text file (UTF-8) and return the number of characters written.
+    Write a string to a text file (UTF-8).
 
     :param filename: Name of the file to be written.
     :param text: String to be written to the file.
@@ -8,5 +9,4 @@ def write_file(filename="", text=""):
     """
     with open(filename, mode="w", encoding="utf-8") as file:
         num_characters = file.write(text)
-    
     return num_characters
