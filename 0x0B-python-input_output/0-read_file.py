a08e70ed0eed18acc57a83c8a8e784192e727860
Hussein <akanjiabayomi2@gmail.com>
2024-01-09 09:12:49 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
+"""Defines a text file-reading function."""
+
 def read_file(filename=""):
-    """
-    Read the content of a UTF-8 encoded text file and print it to stdout.
-    :param filename: Name of the file to be read.
-    """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
